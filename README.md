6d22b6bf13fa3bf6cc2a03e498298c39a65b22ea
SujayShanbhag <sujay.shanbhag999@gmail.com>
2023-08-06 20:30:28 +0530

    read me

---
+++ README.md
@@ -0,0 +1,1 @@
+Hello read me test
--- build/app/user/queries.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.queries = void 0;
-exports.queries = `#graphql
-    verifyGoogleToken(token : String!) : String
-    getCurrentUser: User
-`;
